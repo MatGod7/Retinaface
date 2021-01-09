66e36cd4c3aa95f83c10e1fde4c14600398b0a8a
MatGod7 <49246264+MatGod7@users.noreply.github.com>
2021-01-09 00:14:25 -0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -40,3 +40,42 @@ cfg_re50 = {
     'out_channel': 256
 }
 
+cfg_sls60 = {
+    'name': 'selecsls60',
+    'min_sizes': [[16, 32], [64, 128], [256, 512]],
+    'steps': [8, 16, 32],
+    'variance': [0.1, 0.2],
+    'clip': False,
+    'loc_weight': 2.0,
+    'gpu_train': True,
+    'batch_size': 1,
+    'ngpu': 1,
+    'epoch': 2,
+    'decay1': 70,
+    'decay2': 90,
+    'image_size': 640,
+    'pretrain': True,
+    'return_layers': {'layer2': 1, 'layer3': 2, 'layer4': 3},
+    'in_channel': 32,
+    'out_channel': 1024
+}
+
+cfg_3net_m = {
+    'name': 'tresnet',
+    'min_sizes': [[16, 32], [64, 128], [256, 512]],
+    'steps': [8, 16, 32],
+    'variance': [0.1, 0.2],
+    'clip': False,
+    'loc_weight': 2.0,
+    'gpu_train': True,
+    'batch_size': 1, 
+    'ngpu': 1,
+    'epoch': 2,
+    'decay1': 70,
+    'decay2': 90,
+    'image_size': 640,
+    'pretrain': True,
+    'return_layers': {'layer2': 1, 'layer3': 2, 'layer4': 3},
+    'in_channel': 32,
+    'out_channel': 64
+}

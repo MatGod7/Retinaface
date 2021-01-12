ac92cbaea5c71f8f1d884ef298c05d489b951c70
MatGod7 <49246264+MatGod7@users.noreply.github.com>
2021-01-12 16:29:26 -0300

    Update train.py

--- train.py
+++ train.py
@@ -84,8 +84,7 @@ cudnn.benchmark = True
 
 optimizer = optim.SGD(net.parameters(), lr=initial_lr, momentum=momentum, weight_decay=weight_decay)
 criterion = MultiBoxLoss(num_classes, 0.35, True, 0, True, 7, 0.35, False)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=max_epoch)
-scheduler.last_epoch = 170
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=max_epoch, last_epoch=170)
 priorbox = PriorBox(cfg, image_size=(img_dim, img_dim))
 with torch.no_grad():
     priors = priorbox.forward()
